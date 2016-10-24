4fc96c685a37197b554480f9ae3f81d7fd6a586d
Thomas Justin Krueger <Tomjk@acdmail.com>
2016-10-24 16:16:05 -0400

    Create try catch method for cleaner code.

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -20,50 +20,24 @@ namespace Lab01
             double roomHeight;
             Console.WriteLine("Welcome to Grand Circus' Room Detail Generator!");
             Console.WriteLine("All Distances are in feet.");
+
             while (true)
             {
                 Console.WriteLine();
                 Console.Write("Enter Length: ");
                 inputLength = Console.ReadLine();
-
                 Console.Write("Enter Width: ");
                 inputWidth = Console.ReadLine();
-
                 Console.Write("Enter Height: ");
                 inputHeight = Console.ReadLine();
 
-
                 //////////// Processing //////////////
-                //// ----- try to conver length ----- ////
-                try
-                {
-                    roomLength = Convert.ToDouble(inputLength);
-                }
-                catch
-                {
-                    Console.WriteLine("The length you entered was a invalid number, setting to 1 and continuing");
-                    roomLength = 1;
-                }
-                //// ----- try to convert width ------ ////
-                try
-                {
-                    roomWidth = Convert.ToDouble(inputWidth);
-                }
-                catch
-                {
-                    Console.WriteLine("The width you entered was a invalid number, setting to 1 and continuing");
-                    roomWidth = 1;
-                }
-                //// ------ try to convert height ------ /////
-                try
-                {
-                    roomHeight = Convert.ToDouble(inputHeight);
-                }
-                catch
-                {
-                    Console.WriteLine("The height you entered was a invalid number, setting to 1 and continuing");
-                    roomHeight = 1;
-                }
+
+                //// try/catch converting strings to ints ////
+                roomLength = ConvertStringToInt(inputLength);
+                roomWidth = ConvertStringToInt(inputWidth);
+                roomHeight = ConvertStringToInt(inputHeight);
+
                 //////////// Output and Loop ////////////////
 
                 Console.WriteLine();
@@ -79,6 +53,22 @@ namespace Lab01
                 }
                 else break;
             }
+        }
+
+        //try and catch for input
+        public static double ConvertStringToInt(string input)
+        {
+            double convertedString;
+            try
+            {
+                convertedString = Convert.ToDouble(input);
+            }
+            catch
+            {
+                Console.WriteLine("A number you entered was a invalid number, setting to 1 and continuing");
+                convertedString = 1;
+            }
+            return convertedString;
         }
     }
 }
